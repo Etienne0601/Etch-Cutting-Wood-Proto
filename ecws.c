5d501572bb81eeb3a4e2fc4c869cb244de822b86
Etienne Robin <etienner0601@gmail.com>
2022-03-31 21:33:48 -0700

    made some progress, still no working prototype

--- ecws.c
+++ ecws.c
@@ -3,30 +3,213 @@
 #include <string.h>
 #include <sys/types.h>
 
-typedef unsigned int uint_t;
 
-
+/**
+ * A struct to store the problem instance as defined by the user
+ */
 typedef struct {
     int num_stock_types;
     int num_shelf_types;
-    int* stock_lengths;
-    int* stock_costs;
-    int* shelf_lengths;
-    int* shelf_quantities;
+    int* stock_lengths;    // in increasing order
+    int* stock_costs;      // position corresponding to type in stock_lengths
+    int* shelf_lengths;    // in increasing order
+    int* shelf_quantities; // position corresponding to type in shelf_lengths
 } problemspec_t;
 
+/**
+ * A struct for a node in the linked list below
+ * This is the unit that maps the key (cut pattern) to the value (stock type)
+ */
+typedef struct {
+    // the length of cut_pattern is num_shelf_types
+    int* cut_pattern;
+    int stock_type; // a value between 0 and num_stock_type - 1 (inclusive)
+    node_t* next;
+} node_t;
+
+
+/**
+ * A struct for a linked list used in the hash map
+ */
+typedef struct {
+    int size;
+    node_t* front;
+} LL_t;
+
+
+/**
+* A struct to store all the maximal cutting patterns for a given problem
+ * instance, as well as information relevant to build the MIP model.
+ *
+ * A cutting pattern is a vector of length m, where m is num_shelf_types.
+ * Each cutting pattern has an associated cost that it determined from the type
+ * of stock it is cut from.
+ *
+ */
+typedef struct {
+
+    // I need a hash map that maps cutting patterns to stock types
+    // For simplicity right now I'm going to have a fixed capacity of 100 buckets
+
+    int num_stock_types;
+    int num_shelf_types;
+    int size;
+    int capacity;
+    LL_t* buckets;
+
+} hashmap_t;
+
+
+/**
+ * computes the hash of the array of numbers
+ */
+int hash_vect(int * nums, int n) {
+
+    int hash = 17;
+    int i;
+
+    for (i = 0; i < n; ++i) {
+        hash = hash * 19 + nums[i];
+    }
+
+    return hash;
+}
+
+
+/**
+ * Initializes the hashmap
+ *
+ * Returns 0 if successful, or -1 if it failed
+ */
+int init_hashmap(problemspec_t * prob_instance, hashmap_t * hashmap) {
+
+    int i;
+
+    // initialize the buckets array with a capacity of 100
+    hashmap->buckets = (LL_t*) malloc(100 * sizeof(LL_t));
+    hashmap->capacity = 100;
+    hashmap->size = 0;
+    hashmap->num_stock_types = prob_instance->num_stock_types;
+    hashmap->num_shelf_types = prob_instance->num_shelf_types;
+
+    if (hashmap->buckets == NULL) {
+        fprintf(stderr, "malloc failed in init_hashmap\n");
+        return -1;
+    }
+
+    for (i = 0; i < 100; ++i) {
+        hashmap->buckets[i].size = 0;
+        hashmap->buckets[i].front = NULL;
+    }
+
+    return 0;
+}
+
+
+/**
+ * Inserts a cutting pattern and stock type into the hashmap.
+ * If the cutting pattern already exists in the hashmap, then replace the stock
+ * type with the new stock type if the new one is cheaper.
+ *
+ * Returns 0 if successful, or -1 if it failed
+ */
+int insert_hashmap(hashmap_t * hashmap, int * cut_pattern, int stock_type) {
+
+    int hash, i;
+    node_t* new_node;
+
+    hash = hash_vect(cut_pattern, hashmap->num_shelf_types);
+    hash %= 100;
+
+    new_node = (node_t*) malloc(sizeof(node_t));
+
+    if (hashmap->buckets[hash].size == 0) {
+        // there's no elements in this bucket so cut_pattern doesn't yet exist
+        // in the hashmap
+
+
+        if (new_node == NULL) {
+            fprintf(stderr, "malloc failed in insert_hashmap\n");
+            return -1;
+        }
+
+        new_node->cut_pattern = cut_pattern;
+        new_node->stock_type = stock_type;
+        new_node->next = NULL;
+        hashmap->buckets[hash].front = new_node;
+        ++hashmap->buckets[hash].size;
+        ++hashmap->size;
+    }
+
+    return 0;
+}
+
+
+/**
+ * takes the dot product of two vectors of length n
+ *
+ * Returns 0 if successful, or -1 if it failed
+ */
+int dot_product(int * a, int * b, int n) {
+    int i;
+    int result = 0;
+
+    if (n < 1) {
+        fprintf(stderr, "Dot Product Error!\n");
+        return -1;
+    }
+
+    for (i = 0; i < n; ++i) {
+        result += a[i] * b[i];
+    }
+
+    return result;
+}
+
+
+/**
+ * This function generates all the maximal cutting patterns available with the
+ * given prob_instance and stores them in cut_patterns. Redundant maximal
+ * cutting patterns that arise due to different stock sizes are ignored, and
+ * the cheaper stock size for a given cutting pattern is preferred.
+ *
+ * How should I deal with these redundant maximal cutting patterns?
+ * How about a map whose keys are the maximal cutting pattern ID and the values
+ * are the corresponding cheapest stock type for that cutting pattern found so
+ * far. Therefore, if the same maximal cutting pattern comes up again with a
+ * different stock type, the value will be replaced with the new stock type if
+ * it is cheaper.
+ *
+ * Returns 0 if successful, or -1 if it failed
+ */
+int cutting_patterns_gen(problemspec_t * prob_instance, hashmap_t * cut_patterns) {
+
+    // iteration vars
+    int i, j;
+
+    /** loop over the different stock types to generate the maximal cutting
+     *  patterns for each
+     */
+    for (i = 0; i < prob_instance->num_stock_types; ++i) {
+
+    }
+
+    return 0;
+}
 
 /**
  * process input file to get the data in the format we want
  *
  * k will be an int type
  * m with be an int type
- * the stock lengths will be int array
+ * the stock lengths will be int array in increasing order
  *     (will be later extended to fractional lengths)
- * the costs will be will be uint_t array (in cents)
- * the shelf lengths will be int array
+ * the costs will be will be int array (in cents) in order corresponding to the
+ *     indices in the stock lengths array
+ * the shelf lengths will be int array in increasing order
  *     (will be later extended to fractional lengths)
- * the demanded quantity of shelves will be int array
+ * the demanded quantity of shelves will be int array in order corresponding to
+ *     the indices in the shelf lengths array
  *
  * stores the problemspec instance in prob_instance
  *
@@ -34,9 +217,9 @@ typedef struct {
  * are dynamically allocated in this function and it's the responsiblity of the
  * caller to free
  *
- * Returns 0 if successful, or -1 if failed
+ * Returns 0 if successful, or -1 if it failed
  */
-int parse_input(FILE * in_file, problemspec_t * prob_instance) {
+int parse_input(problemspec_t * prob_instance, FILE * in_file) {
 
     char* line = NULL;  // to hold string output from getline
     size_t len = 0;
@@ -68,6 +251,7 @@ int parse_input(FILE * in_file, problemspec_t * prob_instance) {
     k = atoi(line);
     if (k == 0) {
         // atoi failed to convert the string to an integer
+        fprintf(stderr, "atoi failed to convert string to integer\n");
         // free the buffer allocated by getline
         free(line);
         return -1;
@@ -98,6 +282,7 @@ int parse_input(FILE * in_file, problemspec_t * prob_instance) {
     m = atoi(line);
     if (m == 0) {
         // atoi failed to convert the string to an integer
+        fprintf(stderr, "atoi failed to convert string to integer\n");
         // free the buffer allocated by getline
         free(line);
         return -1;
@@ -116,6 +301,15 @@ int parse_input(FILE * in_file, problemspec_t * prob_instance) {
     prob_instance->stock_costs      = (int*) malloc(k * sizeof(int));
     prob_instance->shelf_lengths    = (int*) malloc(m * sizeof(int));
     prob_instance->shelf_quantities = (int*) malloc(m * sizeof(int));
+
+    if (prob_instance->stock_lengths       == NULL
+        || prob_instance->stock_costs      == NULL
+        || prob_instance->shelf_lengths    == NULL
+        || prob_instance->shelf_quantities == NULL) {
+
+        fprintf(stderr, "malloc failed in parse_input\n");
+        return -1;
+    }
 
     // ========================================================================
     // read the third line from the input file
@@ -244,7 +438,7 @@ int parse_input(FILE * in_file, problemspec_t * prob_instance) {
     }
 
     // debugging output
-    printf("Retrieved the sixth line of length %d\n", nread);
+    printf("Retrieved the sixth line of length %ld\n", nread);
 
     // we need to capture all m shelf quantities
     numptr = line;
@@ -302,7 +496,7 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    parse_input(fp, &prob_instance);
+    parse_input(&prob_instance, fp);
 
     temp = fclose(fp);
     if (temp != 0) {
